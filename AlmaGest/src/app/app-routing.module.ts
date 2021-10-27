7922297fa478a350a1eae0b5e51eb151124e7bac
Komuraptor <50923257+Komuraptor@users.noreply.github.com>
2021-10-27 10:47:14 +0200

    Login commit 1
    Estilo login

--- AlmaGest/src/app/app-routing.module.ts
+++ AlmaGest/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
