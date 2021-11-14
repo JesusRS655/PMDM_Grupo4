3ae43646d6a8cb0906e538249ab7ddd8505b016e
Jesus <jesusrs655@gmail.com>
2021-11-14 18:20:30 +0100

    Refresher
    Últimos cambios y refresher añadido

--- AlmaGest/src/app/pages/admin/admin.page.ts
+++ AlmaGest/src/app/pages/admin/admin.page.ts
@@ -2,7 +2,7 @@ import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { AlertController, IonList, ModalController } from "@ionic/angular";
 import { DataService } from "src/app/services/data.service";
-import { EditarPage } from '../editar/editar.page';
+import { EditarPage } from "../editar/editar.page";
 
 @Component({
   selector: "app-admin",
@@ -10,11 +10,16 @@ import { EditarPage } from '../editar/editar.page';
   styleUrls: ["./admin.page.scss"],
 })
 export class AdminPage implements OnInit {
-
-  @ViewChild('lista', {static: true}) lista: IonList;
+  @ViewChild("lista", { static: true }) lista: IonList;
   usuarios: any[] = [];
 
-  constructor(public router: Router, private dataService: DataService, private modalCtrl: ModalController, public alertCtrl: AlertController, public changer: ChangeDetectorRef) {
+  constructor(
+    public router: Router,
+    private dataService: DataService,
+    private modalCtrl: ModalController,
+    public alertCtrl: AlertController,
+    public changer: ChangeDetectorRef
+  ) {
     if (router.getCurrentNavigation().extras.state) {
       const token = this.router.getCurrentNavigation().extras.state;
       // console.log(token);
@@ -45,43 +50,53 @@ export class AdminPage implements OnInit {
   async abrirEditar(usuario) {
     const modal = await this.modalCtrl.create({
       component: EditarPage,
-      componentProps:{
-        usuario: usuario
-      }
+      componentProps: {
+        usuario: usuario,
+      },
     });
     await modal.present();
     const { data } = await modal.onDidDismiss();
-    console.log(data);    
+    console.log(data);
     this.dataService.editar(data.usuario);
     this.lista.closeSlidingItems();
   }
 
   async eliminar(usuario) {
     const alert = await this.alertCtrl.create({
-      header: 'Alerta',
-      subHeader: 'Borrar usuario',
-      message: '¿Esta seguro de querer borrar el usuario?',
-      buttons: [{
-        text: 'No',
-        role: 'cancel',
-        handler: () => {
-          this.lista.closeSlidingItems();
-        }
-      },{
-        text: 'Si',
-        handler: () => {
-          this.dataService.eliminar(usuario);
-          this.recargar();
-          console.log("eliminar", usuario);
-        }
-      }]
+      header: "Alerta",
+      subHeader: "Borrar usuario",
+      message: "¿Esta seguro de querer borrar el usuario?",
+      buttons: [
+        {
+          text: "No",
+          role: "cancel",
+          handler: () => {
+            this.lista.closeSlidingItems();
+          },
+        },
+        {
+          text: "Si",
+          handler: () => {
+            this.dataService.eliminar(usuario);
+            this.recargar();
+            console.log("eliminar", usuario);
+          },
+        },
+      ],
     });
     await alert.present();
   }
 
-  recargar(){
+  recargar() {
     this.ngOnInit();
-    this.router.navigateByUrl('/tabs/admin')
-    console.log('recarga');
+    this.router.navigateByUrl("/tabs/admin");
+    console.log("recarga");
+  }
+
+  refrescar(event) {
+    setTimeout(() => {
+      this.recargar();
+      event.target.complete();
+    }, 1500);
   }
 }
