a9c0036bbcd052b21fe2d3bbd8794f02ac78343e
Komuraptor <50923257+Komuraptor@users.noreply.github.com>
2022-02-14 10:49:32 +0100

    Commit

--- AlmaGest/src/app/pages/grafica/grafica.module.ts
+++ AlmaGest/src/app/pages/grafica/grafica.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { GraficaPageRoutingModule } from './grafica-routing.module';
 
 import { GraficaPage } from './grafica.page';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgChartsModule,
     GraficaPageRoutingModule
   ],
   declarations: [GraficaPage]
--- AlmaGest/src/app/pages/grafica/grafica.page.ts
+++ AlmaGest/src/app/pages/grafica/grafica.page.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
+
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-grafica',
@@ -7,6 +12,7 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./grafica.page.scss'],
 })
 export class GraficaPage implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   constructor(
     private dataService: DataService
@@ -15,11 +21,80 @@ export class GraficaPage implements OnInit {
   // https://valor-software.com/ng2-charts/#BarChart
 
   pedidos: any[] = [];
+  currentDate = new Date();
+  meses = [
+    (this.currentDate.getMonth())+'/'+this.currentDate.getFullYear()
+  ];
 
+  
   ngOnInit() {
     this.dataService.getPedidos().then((data) => {
       this.pedidos = data.data;
     })
+
+    for (let i = 0; i < 6; i++) {
+      if((this.currentDate.getMonth()-i)<0){
+        this.meses[i]=(this.currentDate.getMonth()+(11-i))+'/'+(this.currentDate.getFullYear()-1);
+      }
+      else {
+        this.meses[i]=(this.currentDate.getMonth()+i)+'/'+this.currentDate.getFullYear();
+      }
+    }
+  }
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [ this.meses[0], this.meses[1], this.meses[2], this.meses[3], this.meses[4], this.meses[5]],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55 ], label: 'Ventas' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
+
+    this.chart?.update();
   }
 
 }
